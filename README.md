59f6900dc8dd5bdf2a8c7072d28e34f43301d830
syj <syj990930@naver.com>
2021-07-17 16:16:30 +0900

    third123123123

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 "# algorithm" 
 "# algorithm" 
+
+
+# 설치 순서
+1. 노트북에 git 설치 (프로그램만)
+2. git clone (원하는 위치에)
+3. intellij에 import (프로젝트 불러오기)
+
--- src/GitTestSecondEdition.java
+++
@@ -1,9 +0,0 @@
-public class GitTestSecondEdition {
-    public static void main(String[] args) {
-        aaaaa
-    }
-
-    void test() {
-        System.out.println("test");
-    }
-}
