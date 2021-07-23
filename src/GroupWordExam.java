7d19c2159930a8f9fc37e723179819126af64f27
syj <syj990930@naver.com>
2021-07-23 19:12:47 +0900

    뿌엥

--- src/GroupWordExam.java
+++ src/GroupWordExam.java
@@ -4,6 +4,8 @@ public class GroupWordExam {
     public static void main(String[] args) {
         //문자가 연속해서만 나타나는 경우를 그룹단어라고 함
         //단어N개를 입력받아 그룹단어의 개수를 출력하는 프로그램 작성
+
+        //모르겠다 !!!!!!!!!!!
         char[] alphabetArray = new char[26];
         for (int i = 0; i < alphabetArray.length; i++) {
             alphabetArray[i] = (char) ('a' + i);
